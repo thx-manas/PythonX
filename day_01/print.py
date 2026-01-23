20a688ab2d60790763a0e5fc1aa702ba07dceb56
thx-manas <thxmanassingh@gmail.com>
2026-01-23 22:05:53 +0530

    Usage of print statement

---
+++ day_01/print.py
@@ -0,0 +1,22 @@
+print(3+2) #addition
+print(5-1) #subtration
+print(8*3) #multiplication
+print(12/4) #division
+print(9**2) #exponential
+print(9 // 2) #floor devision
+print(7 % 2) #modulus or remainder
+
+#written above are basic operations
+
+print(type(10))                      # Int
+print(type(3.14))                    # Float
+print(type(1 + 3j))                  # Complex
+print(type('Manas'))              # String
+print(type([1, 2, 3]))               # List
+print(type({'name':'Manas'}))     # Dictionary
+print(type({9.8, 3.14, 2.7}))        # Set
+print(type((9.8, 3.14, 2.7)))        # Tuple
+print(type(3 == 3))                  # Boolean
+print(type(3 >= 3))                  # Boolean data type
+
+#the second set is data types
